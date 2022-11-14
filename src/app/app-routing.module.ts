f1ee594b9de96a2dfe29af28566d4841dfefbcf0
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-11-14 18:19:55 -0500

    set route for registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from "path";
+import {RegistrationComponent} from "./auth/registration/registration.component";
+import {LandingComponent} from "./landing/landing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LandingComponent},
+  {path: "signup", component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
