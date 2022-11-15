0e234675d26dde32a4a41f52800aed983f63df73
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-11-15 17:02:07 -0500

    component create and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import * as path from "path";
 import {RegistrationComponent} from "./auth/registration/registration.component";
 import {LandingComponent} from "./landing/landing.component";
+import {DietListComponent} from "./diet/diet-list/diet-list.component";
+import {ExerciseListComponent} from "./exercise/exercise-list/exercise-list.component";
+import {ExpertListComponent} from "./expert/expert-list/expert-list.component";
+import {DietPlanComponent} from "./diet/diet-plan/diet-plan.component";
+import {ExercisePlanComponent} from "./exercise/exercise-plan/exercise-plan.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   {path: "", component: LandingComponent},
-  {path: "signup", component: RegistrationComponent}
+  {path: "signup", component: RegistrationComponent},
+  {path: "diet", component: DietListComponent},
+  {path: "dietPlan", component: DietPlanComponent},
+  {path: "exercise", component: ExerciseListComponent},
+  {path: "exercisePlan", component: ExercisePlanComponent},
+  {path: "expert", component: ExpertListComponent},
+  {path: "about", component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
+import { ExerciseListComponent } from './exercise/exercise-list/exercise-list.component';
+import { ExercisePlanComponent } from './exercise/exercise-plan/exercise-plan.component';
+import { DietPlanComponent } from './diet/diet-plan/diet-plan.component';
+import { DietListComponent } from './diet/diet-list/diet-list.component';
+import { ExpertListComponent } from './expert/expert-list/expert-list.component';
+import { ExpertDetailComponent } from './expert/expert-detail/expert-detail.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    ExerciseListComponent,
+    ExercisePlanComponent,
+    DietPlanComponent,
+    DietListComponent,
+    ExpertListComponent,
+    ExpertDetailComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
