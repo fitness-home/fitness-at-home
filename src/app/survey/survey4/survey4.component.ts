68285abb1d35a0f4fb439c117eea3eeb5b5b646f
Chohee Noh <cnoh1@my.centennialcollege.ca>
2022-11-17 21:09:07 -0500

    Working on it
    exercise/survey

---
+++ src/app/survey/survey4/survey4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey4',
+  templateUrl: './survey4.component.html',
+  styleUrls: ['./survey4.component.scss']
+})
+export class Survey4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/survey/survey6/survey6.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey6',
+  templateUrl: './survey6.component.html',
+  styleUrls: ['./survey6.component.scss']
+})
+export class Survey6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
