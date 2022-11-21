6ddffba1389e0cb39dfb9ae465d2b50390f6ce38
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-11-20 21:10:38 -0500

    Code for view diet plan list with api integration and solved design issue

--- src/app/diet/diet-list/diet-list.component.ts
+++ src/app/diet/diet-list/diet-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DietService} from "../diet.service";
 
 @Component({
   selector: 'app-diet-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diet-list.component.scss']
 })
 export class DietListComponent implements OnInit {
-
-  constructor() { }
+  dietList: any;
+  constructor(public dietService: DietService) { }
 
   ngOnInit(): void {
+    this.onGetDietList();
+  }
+
+  onGetDietList() {
+    this.dietService.getDietList().subscribe((res) =>  {
+      this.dietList = res;
+      console.log("diet", this.dietList);
+    })
   }
 
 }
