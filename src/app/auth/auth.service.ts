5502671bb0c8da4dbcd996cc2a76f94c46b22f92
Abasibiangake James <jamesabasibiangake@gmail.com>
2022-12-07 20:00:30 -0500

    frontend login code Update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,61 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, of } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 import {environment} from "../../environments/environment";
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public http: HttpClient) { }
+  public username?: string;
+  private _redirectUrl?: string| null;
+  baseUrl: string;
+  auth_token?: string;
+
+  constructor(public http: HttpClient) { 
+    this.baseUrl = environment.apiUrl;
+        console.log(this.baseUrl);
+  }
 
   createUser(user: any): Observable<Object> {
     return this.http.post(`${environment.apiUrl}/user`, user);
   }
+  
+  authenticate(user: string, pass: string): Observable<any> {
+    return this.http.post<any>(this.baseUrl + "/signin", 
+    {
+        username: user, 
+        password: pass
+    }).pipe(
+        map(response => {
+            this.auth_token = response.username ? response.token : null;
+            return response;
+        }),
+        catchError(error => {return of(error.error)})
+    );
+}
+
+get authenticated(): boolean {
+  return this.auth_token != null;
+}
+
+
+get redirectUrl(): string{
+  let result = this._redirectUrl;
+  this._redirectUrl = null;
+  return result as string;
+}
+set redirectUrl(url: string){
+  this._redirectUrl = url;
+}
+private provideToken() {
+  return {
+      headers: new HttpHeaders({
+          "Authorization": `Bearer ${this.auth_token}`
+      })
+  }
+}
 }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import { NgForm } from "@angular/forms";
+import {AuthService} from "../auth.service";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-signin',
@@ -6,8 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  public username: any
+  public password: any;
+  public message?: string;
 
-  constructor() { }
+  constructor(private router: Router,
+    private auth: AuthService) { }
+
+authenticate(form: NgForm) {
+    if (form.valid) {
+
+        // perform authentication
+        this.auth.authenticate(this.username, this.password)
+            .subscribe(response => {
+                if (response.username) {
+                    this.router.navigateByUrl(this.auth.redirectUrl || "/");
+                }
+                this.message = "login success";
+            });
+    } else {
+        this.message = "Form Data Invalid";
+    }
+}
 
   ngOnInit(): void {
   }
